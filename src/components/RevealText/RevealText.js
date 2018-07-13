1102c3e46d0f5b859c07af32207debe8220516bd
Julius Sohn <juliussohn@icloud.com>
2018-07-13 12:33:56 +0200

    reveal text

---
+++ src/components/RevealText/RevealText.js
@@ -0,0 +1,46 @@
+import CSSModules from 'react-css-modules';
+import React, { Component } from 'react';
+import styles from './RevealText.scss'
+
+import VisibilitySensor from 'react-visibility-sensor';
+
+
+class RevealText extends Component {
+
+	constructor(props) {
+		super(props);
+		this.textFragments = props.text.split(' ');
+		this.state = {
+			revealed: false
+		}
+
+		this.onChangeVisibility = this.onChangeVisibility.bind(this)
+	}
+
+	onChangeVisibility(visibility) {
+		if (this.state.revealed == false && visibility) {
+			this.setState({
+				revealed: true
+			})
+		}
+	}
+
+
+
+	render() {
+		const { revealed } = this.state;
+		return (
+			<React.Fragment>
+				<VisibilitySensor onChange={this.onChangeVisibility} />
+				{this.textFragments.map((fragment, index) =>
+					<React.Fragment>
+						<span styleName={`fragment${revealed == true ? '-revealed' : ''}`} style style={{ 'transition-delay': `${index * 0.1}s` }}>{fragment + ' '}</span>  {' '}
+					</React.Fragment>)}
+			</React.Fragment>
+		)
+	}
+}
+
+
+
+export default CSSModules(RevealText, styles);
