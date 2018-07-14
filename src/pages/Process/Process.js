5ab5ca2d25bcf2e82faf9289e709ab49729754af
Julius Gehrig <hello@julius.fm>
2018-07-14 13:45:03 +0200

    Add fonts & typography fixes

--- src/pages/Process/Process.js
+++ src/pages/Process/Process.js
@@ -13,7 +13,7 @@ const Process = (props) => (
     <h1 styleName="pageTitle">
       <RevealText text="Design Process"></RevealText>
     </h1>
-    <h3 styleName="subheadline"><RevealText text="How did we get here?"></RevealText></h3>
+    <h3 styleName="subheadline"><RevealText text="How did we get here"></RevealText></h3>
     <div class="row center-xs">
       <div class="col-xs-10">
         <div class="box">
